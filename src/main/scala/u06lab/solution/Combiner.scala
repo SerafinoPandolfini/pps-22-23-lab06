807c611037e0424078e43ce96ce08d84d5f438d2
Serafino Pandolfini <serafino.pandolfini@studio.unibo.it>
2023-04-19 16:03:55 +0200

    Add implementations and tests for Combiner

--- src/main/scala/u06lab/solution/Combiner.scala
+++ src/main/scala/u06lab/solution/Combiner.scala
@@ -6,13 +6,13 @@ trait Functions:
   def sum(a: List[Double]): Double
   def concat(a: Seq[String]): String
   def max(a: List[Int]): Int // gives Int.MinValue if a is empty
+  def combiner[A](a: Seq[A], b: Combiner[A]): A
 
 object FunctionsImpl extends Functions:
-  override def sum(a: List[Double]): Double = a.foldLeft(0.0)((d1, d2) => d1 +d2)
-  override def concat(a: Seq[String]): String = a.foldLeft("")((s1, s2) => s1.concat(s2))
-  override def max(a: List[Int]): Int = a match
-    case Nil => Int.MinValue
-    case _ => a.max
+  override def sum(a: List[Double]): Double = a.foldLeft(0.0)(_ + _)
+  override def concat(a: Seq[String]): String = a.foldLeft("")(_.concat(_))
+  override def max(a: List[Int]): Int = a.foldLeft(Int.MinValue)(_.max(_))
+  override def combiner[A](a: Seq[A], b: Combiner[A]): A = a.foldLeft(b.unit)(b.combine)
 
 /*
  * 2) To apply DRY principle at the best,
@@ -30,6 +30,19 @@ object FunctionsImpl extends Functions:
 trait Combiner[A]:
   def unit: A
   def combine(a: A, b: A): A
+  
+object CombinerSum extends Combiner[Double]:
+  override def unit: Double = 0.0
+  override def combine(a: Double, b: Double): Double = a + b
+  
+object CombinerConcat extends Combiner[String]:
+  override def unit: String = ""
+  override def combine(a: String, b: String): String = a.concat(b)
+  
+object CombinerMax extends Combiner[Int]:
+  override def unit: Int = Int.MinValue
+  override def combine(a: Int, b: Int): Int = a.max(b)
+  
 
 @main def checkFunctions(): Unit =
   val f: Functions = FunctionsImpl
--- src/test/scala/u06lab/code/Combiner.scala
+++ src/test/scala/u06lab/code/Combiner.scala
@@ -8,11 +8,22 @@ class CombinerTests {
   @Test
   def testFunctions() = {
     val f: Functions = FunctionsImpl
-    assertEquals(60.1, f.sum(List(10.0, 20.0, 30.1)), 0.001) // 60.1
-    assertEquals(0.0, f.sum(List()), 0.001) // 0.0
+    assertEquals(60.1, f.sum(List(10.0, 20.0, 30.1)), 0.001)
+    assertEquals(0.0, f.sum(List()), 0.001)
     assertEquals("abc", f.concat(Seq("a", "b", "c")))
     assertEquals("", f.concat(Seq()))
     assertEquals(3, f.max(List(-10, 3, -5, 0)))
     assertEquals(Integer.MIN_VALUE, f.max(List()))
+  }
+  
+  @Test
+  def testCombiner() = {
+    val f: Functions = FunctionsImpl
+    assertEquals(60.1, f.combiner(List(10.0, 20.0, 30.1), CombinerSum), 0.001)
+    assertEquals(0.0, f.combiner(List(), CombinerSum), 0.001)
+    assertEquals("abc", f.combiner(Seq("a", "b", "c"), CombinerConcat))
+    assertEquals("", f.combiner(Seq(), CombinerConcat))
+    assertEquals(3, f.combiner(List(-10, 3, -5, 0), CombinerMax))
+    assertEquals(Integer.MIN_VALUE, f.combiner(List(), CombinerMax))   
   }
 }
