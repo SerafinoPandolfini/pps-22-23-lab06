7d32dd617be3b4936fc4c915dd070c0dbb030314
Serafino Pandolfini <serafino.pandolfini@studio.unibo.it>
2023-04-24 18:36:21 +0200

    add placeMarks function in Solitaire

--- src/main/scala/u06lab/solution/Solitaire.scala
+++ src/main/scala/u06lab/solution/Solitaire.scala
@@ -1,6 +1,13 @@
 package u06lab.solution
 
+import scala.collection.IterableFactory
+
 object Solitaire extends App:
+  type Cell = (Int, Int)
+  type Solution = Seq[Cell]
+  type IterableFactory = Solution => Iterable[Solution]
+  given IterableFactory = List(_).view
+
   def render(solution: Seq[(Int, Int)], width: Int, height: Int): String =
     val reversed = solution.reverse
     val rows =
@@ -11,5 +18,27 @@ object Solitaire extends App:
       yield row.mkString
     rows.mkString("\n")
 
+  def placeMarks(w: Int, h: Int)(using factory : IterableFactory): Iterable[Solution] =
+    def _placeMarks(s: Int): Iterable[Solution] = s match
+      case 1 => factory(Seq((w / 2, h / 2)))
+      case _ =>
+        for
+          sequence <- _placeMarks(s - 1)
+          x <- 0 until w
+          y <- 0 until h
+          newCell = (x, y)
+          if !sequence.contains(newCell) && isCellAdjacent(newCell, sequence.head)
+        yield
+          Seq(newCell).appendedAll(sequence)
+    _placeMarks(w * h)
 
-  println(render(solution = Seq((0, 0), (2, 1)), width = 3, height = 3))
+  def isCellAdjacent(cell: Cell, last: Cell): Boolean =
+    (cell._1 == last._1 && (cell._2 - last._2).abs.equals(2)) ||
+      (cell._2 == last._2 && (cell._1 - last._1).abs.equals(2)) ||
+      ((cell._1 - last._1).abs.equals(1) && (cell._2 - last._2).abs.equals(1))
+
+  val w = 1
+  val h = 1
+  val solutions = placeMarks(w, h)
+  solutions.foreach(sol => println("\n"+render(solution = sol, width = w, height = h)))
+  println("There are " + solutions.size + " solutions")
